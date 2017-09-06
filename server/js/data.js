ae041333703bae4e331e7d742f7265a6fbb079f3
Colwyn Fritze-Moor <colwyn.myself@gmail.com>
2017-09-05 22:21:21 -0700

    redux: Added resume command to API

--- server/js/data.js
+++ server/js/data.js
@@ -1,9 +1,17 @@
 const responseHash = {
-    help: 'Currently you can only run three commands, \"help\", \"source\", and \"about\". I\'ll add more commands and \
-        interactivity soon.',
-    about: 'This is my personal site/project. Over time it will become my resume and also how I manage many of my \
-        tools.',
-    source: '<a href=\"https://github.com/colwynmyself/colwyn.me\" target=\"_blank\">https://github.com/colwynmyself/colwyn.me</a>',
+    help: `Suggested commands: about, source, resume<br>Try whatever you like however, other commands are being implemented over time.`,
+    about: `This is my personal site/project. Over time it will become my resume and also how I manage many of my \
+        tools.`,
+    source: `<a href="https://github.com/colwynmyself/colwyn.me" target="_blank">https://github.com/colwynmyself/colwyn.me</a>`,
+    resume: `Colwyn Fritze-Moor -> Pop Art, Inc. [Software Developer]<br>
+        Email: <a href="mailto:colwyn.myself@gmail.com" title="Email" class="terminal-link">colwyn.myself@gmail.com</a><br>
+        GitHub: <a href="https://github.com/colwynmyself" title="GitHub" class="terminal-link" target="_blank">colwynmyself</a><br>
+        Career: Pop Art, Inc. [Software Developer], Glass Bead, Inc. [Front End Developer], AutoBidMaster [Web Graphic Designer]<br>
+        Languages: JavaScript (ES6), TypeScript, Python, C#, SQL<br>
+        Databases: Postgres, MongoDB<br>
+        Frameworks/Runtimes: Node.JS, .NET<br>
+        Libraries: React, VueJS, Express, Koa, Mongoose, Sequelize<br>
+        Server Tools: Nginx, Docker`,
 };
 
 module.exports = {
