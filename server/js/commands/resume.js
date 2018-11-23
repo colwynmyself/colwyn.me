57555fe4f20266761c166c668a0260c7c6eb3c64
Colwyn Fritze-Moor <colwyn.fritze-moor@legitscript.com>
2018-11-22 19:58:08 -0800

    Update resume.js

--- server/js/commands/resume.js
+++ server/js/commands/resume.js
@@ -1,10 +1,10 @@
 module.exports = () => {
-    return `Colwyn Fritze-Moor -> Pop Art, Inc. [Software Developer]<br>
+    return `Colwyn Fritze-Moor -> LegitScript [Software Engineer]<br>
         Email: <a href="mailto:colwyn@colwyn.me" title="Email" class="terminal-link">colwyn@colwyn.me</a><br>
         GitHub: <a href="https://github.com/colwynmyself" title="GitHub" class="terminal-link" target="_blank">colwynmyself</a><br>
-        Career: Pop Art, Inc. [Software Developer], Glass Bead, Inc. [Front End Developer], AutoBidMaster [Web Graphic Designer]<br>
-        Languages: JavaScript (ES6), TypeScript, Python, C#, SQL<br>
-        Databases: Postgres, MongoDB<br>
+        Career: LegitScript [Software Engineer], Pop Art, Inc. [Software Developer], Glass Bead, Inc. [Front End Developer], AutoBidMaster [Web Graphic Designer]<br>
+        Languages: Python, JavaScript (ES6), TypeScript, C#, SQL<br>
+        Databases: MySQL, Postgres, MongoDB<br>
         Frameworks/Runtimes: Node.JS, .NET<br>
         Libraries: React, VueJS, Express, Koa, Mongoose, Sequelize<br>
         Server Tools: Nginx, Docker`;
