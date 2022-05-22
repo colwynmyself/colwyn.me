064550e667adfcb9c13629d4af4d4c646716508b
Colwyn Fritze-Moor <colwyn@colwyn.me>
2022-05-22 16:13:22 -0700

    feat: add pa11y

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,71 @@ import { minify } from 'html-minifier';
 import { readFile, writeFile, readdir, lstat, access, mkdir } from 'fs/promises';
 import { join, dirname } from 'path';
 
+enum logLevels {
+  TRACE = 0,
+  DEBUG = 1,
+  INFO = 2,
+  WARNING = 3,
+  ERROR = 4,
+  CRITICAL = 5,
+}
+
+class Logger {
+  private logLevel: number;
+
+  constructor(logLevel: string = process.env.LOG_LEVEL || 'INFO') {
+    if (!Object.keys(logLevels).includes(logLevel)) {
+      throw new Error(`Log level ${logLevel} is invalid. Must be one of: ${Object.keys(logLevels).join(', ')}`);
+    }
+
+    // Magic type coersion https://stackoverflow.com/a/41970976
+    this.logLevel = logLevels[logLevel as keyof typeof logLevels];
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  private log(level: number, message: string, extras: any[]) {
+    if (this.logLevel <= level) {
+      if (extras.length) {
+        console.log(message, ...extras);
+      } else {
+        console.log(message);
+      }
+    }
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  public trace(message: string, ...extras: any[]) {
+    this.log(logLevels.TRACE, message, extras);
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  public debug(message: string, ...extras: any[]) {
+    this.log(logLevels.DEBUG, message, extras);
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  public info(message: string, ...extras: any[]) {
+    this.log(logLevels.INFO, message, extras);
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  public warning(message: string, ...extras: any[]) {
+    this.log(logLevels.WARNING, message, extras);
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  public error(message: string, ...extras: any[]) {
+    this.log(logLevels.ERROR, message, extras);
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  public critical(message: string, ...extras: any[]) {
+    this.log(logLevels.CRITICAL, message, extras);
+  }
+}
+
+const logger = new Logger();
+
 async function readFileAsString(path: string) {
   return (await readFile(path)).toString('utf8');
 }
@@ -20,7 +85,7 @@ async function fileTree(path: string, isRoot = false): Promise<TreeBranch> {
   const children = isDirectory ? await Promise.all((await readdir(path)).map(childPath => fileTree(join(path, childPath)))) : [];
   const isRenderable = !isRoot && !!children.find(child => child.path.endsWith('index.handlebars'));
 
-  console.log(`${path} - Directory: ${isDirectory} - Renderable: ${isRenderable}.`);
+  logger.debug(`${path} - Directory: ${isDirectory} - Renderable: ${isRenderable}.`);
 
   return {
     children,
@@ -79,18 +144,17 @@ async function main() {
   });
 
   const tree = await fileTree('src/', true);
-  console.log(tree);
   let treeStack: TreeBranch[] = [tree];
 
   while (treeStack.length) {
-    console.log(`Stack length: ${treeStack.length}`);
+    logger.trace(`Stack length: ${treeStack.length}`);
 
     const branch = treeStack.pop();
     if (!branch) {
       break;
     }
 
-    console.log(`Processing ${branch.path} - Renderable: ${branch.isRenderable}`);
+    logger.info(`Processing ${branch.path} - Renderable: ${branch.isRenderable}`);
 
     treeStack = treeStack.concat(branch.children);
 
@@ -103,9 +167,9 @@ async function main() {
 (async () => {
   try {
     await main();
-    console.log(`Completed building at ${new Date().toISOString()}`);
+    logger.info(`Completed building at ${new Date().toISOString()}`);
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
   } catch (e: any) {
-    console.error(`Error building project: ${e.message}`);
+    logger.critical(`Error building project: ${e.message}`);
   }
 })();
