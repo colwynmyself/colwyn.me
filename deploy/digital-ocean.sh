ba96a2c82aad95518793cf9482f937b7177a6514
Colwyn Fritze-Moor <colwyn@colwyn.me>
2022-05-23 22:59:00 -0700

    feat: only deploy on main, better logging

--- deploy/digital-ocean.sh
+++ deploy/digital-ocean.sh
@@ -17,12 +17,18 @@ if [[ -z "${DIGITALOCEAN_TOKEN}" || -z "${DIGITALOCEAN_APP_ID}" ]]; then
   exit 1
 fi
 
+function log() {
+  echo "$(date +'%Y-%m-%d %T') - ${1}"
+}
+
 DEPLOYMENT=$(curl -s -X POST \
   -H "Content-Type: application/json" \
   -H "Authorization: Bearer ${DIGITALOCEAN_TOKEN}" \
   "https://api.digitalocean.com/v2/apps/${DIGITALOCEAN_APP_ID}/deployments"
 )
 DEPLOYMENT_ID=$(echo "${DEPLOYMENT}" | jq -r '.deployment.id')
+
+log "Deployment ${DEPLOYMENT_ID} created."
 
 while true; do
   STATE=$(curl -s -X GET \
@@ -33,13 +39,13 @@ while true; do
   PHASE=$(echo "${STATE}" | jq -r '.deployment.phase')
 
   if [[ "${PHASE}" == "PENDING_BUILD" || "${PHASE}" == "DEPLOYING" ]]; then
-    echo "Waiting for deployment to finish. Current in phase ${PHASE}"
+    log "Waiting for deployment to finish. Current in phase ${PHASE}"
     sleep 1
   elif [[ "${PHASE}" == "ACTIVE" ]]; then
-    echo "Deploy is done and successful!"
+    log "Deployment ${DEPLOYMENT_ID} is done and successful!"
     break
   else
-    echo "Unknown phase ${PHASE}. Assuming this is an error ane exiting. Also here's the whole state."
+    log "Unknown phase ${PHASE}. Assuming this is an error ane exiting. Also here's the whole state."
     echo "${STATE}" | jq
     exit 1
   fi
