9b619f46819312b26e7e79cae2fc619ce090393c
Colwyn Fritze-Moor <colwyn@colwyn.me>
2022-05-25 00:31:45 -0700

    fix: handle errors a little better

--- deploy/digital-ocean.sh
+++ deploy/digital-ocean.sh
@@ -30,23 +30,37 @@ DEPLOYMENT_ID=$(echo "${DEPLOYMENT}" | jq -r '.deployment.id')
 
 log "Deployment ${DEPLOYMENT_ID} created."
 
+ERROR_COUNT=0
 while true; do
   STATE=$(curl -s -X GET \
     -H "Content-Type: application/json" \
     -H "Authorization: Bearer ${DIGITALOCEAN_TOKEN}" \
     "https://api.digitalocean.com/v2/apps/${DIGITALOCEAN_APP_ID}/deployments/${DEPLOYMENT_ID}"
   )
+  ERROR=$(echo "${STATE}" | jq -r '.error')
   PHASE=$(echo "${STATE}" | jq -r '.deployment.phase')
 
-  if [[ "${PHASE}" == "PENDING_BUILD" || "${PHASE}" == "DEPLOYING" ]]; then
+  if [[ "${ERROR}" != "null" ]]; then
+    ERROR_COUNT+=1
+    log "Error number ${NULL_PHASES} encountered. Message: ${ERROR}"
+
+    # This is arbitrary, I just figure more than 3 errors is probably too many
+    # I should really check for what the error is, but that can come later
+    if $ERROR_COUNT gt 3; then
+      log "That's too many errors in a row, exiting"
+      echo "${STATE}" | jq
+      exit 1
+    fi
+  elif [[ "${PHASE}" == "PENDING_BUILD" || "${PHASE}" == "DEPLOYING" || "${PHASE}" == "BUILDING" ]]; then
     log "Waiting for deployment to finish. Current in phase ${PHASE}"
-    sleep 1
   elif [[ "${PHASE}" == "ACTIVE" ]]; then
-    log "Deployment ${DEPLOYMENT_ID} is done and successful!"
+    log "Deployment ${DEPLOYMENT_ID} was successful!"
     break
   else
     log "Unknown phase ${PHASE}. Assuming this is an error ane exiting. Also here's the whole state."
     echo "${STATE}" | jq
     exit 1
   fi
+
+  sleep 1
 done
